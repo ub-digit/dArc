018688884e207e74dd718d5a43382618886009d7
Leonard Norberg <leonard.norberg@sunstone.se>
2014-09-11 16:47:06 +0200

    Added dataformat wrapper for EAC and use it from Authority

--- app/classes/eac_generator.rb
+++ app/classes/eac_generator.rb
@@ -83,4 +83,36 @@ class EacGenerator
 		return schema_validation(xml)
 	end
 
+	# generates an empty EAC document of a specified type. some mandatory structure is present
+	def self.generate_empty(type)
+		builder = Nokogiri::XML::Builder.new do |xml|
+			xml.eac('type' => type, 'xmlns:xsi' => 'http://www.w3.org/2001/XMLSchema-instance', 'xsi:schemaLocation' => 'http://xml.ra.se/EAC/RA_EAC.xsd', 'xmlns' => 'http://xml.ra.se/EAC') {
+				xml.eacheader(status: "draft") {
+					xml.eacid(countrycode: "SE", ownercode: Rails.application.config.ownercode){
+						xml.text('')
+					}
+					xml.mainhist {
+						xml.mainevent(maintype: "create") {
+							xml.maindate Date.today.strftime('%F')
+						}
+					}
+				}
+				xml.condesc {
+					xml.identity {
+						xml.send(type_map[type]+"head", authorized: Rails.application.config.ownercode) {
+							xml.part(type: "forename"){
+								xml.text('')
+							}
+							xml.part(type: "surname"){
+								xml.text('')
+							}
+						}
+					}
+				}
+			}
+		end
+
+		builder.doc
+	end
+
 end
---
+++ app/models/dataformats/eac.rb
@@ -0,0 +1,110 @@
+class Dataformats::EAC < Dataformats::Xml
+
+  def datastream_name
+     'EAC'
+  end
+
+  def authorized_forename= name
+     if is_new?
+       @doc = create_empty
+     end
+     
+     @doc.xpath('//e:eac/e:condesc/e:identity/e:pershead[@authorized!=""]/e:part[@type="forename"]', 'e' => "http://xml.ra.se/EAC")[0].content=name
+  end
+
+  def authorized_forename
+     if is_new?
+       return nil
+     end
+     @doc.xpath('//e:eac/e:condesc/e:identity/e:pershead[@authorized!=""]/e:part[@type="forename"]', 'e' => "http://xml.ra.se/EAC")[0].text
+  end
+
+  def authorized_surname= name
+     if is_new?
+       @doc = create_empty
+     end
+     
+     @doc.xpath('//e:eac/e:condesc/e:identity/e:pershead[@authorized!=""]/e:part[@type="surname"]', 'e' => "http://xml.ra.se/EAC")[0].content=name
+  end
+
+  def authorized_surname
+     if is_new?
+       return nil
+     end
+     @doc.xpath('//e:eac/e:condesc/e:identity/e:pershead[@authorized!=""]/e:part[@type="surname"]', 'e' => "http://xml.ra.se/EAC")[0].text
+  end
+
+  def type= type
+     if is_new?
+       @doc = create_empty
+     end
+
+     nodes = @doc.xpath('//e:eac/@type', 'e' => "http://xml.ra.se/EAC")
+
+     if !type
+       nodes.remove()
+     else
+       nodes[0].value=type
+     end
+  end
+
+  def type
+     if is_new?
+       return nil
+     end
+     @doc.xpath('//e:eac/@type', 'e' => "http://xml.ra.se/EAC")[0].value
+  end
+
+  def startdate= startdate
+     if is_new?
+       @doc = create_empty
+     end
+     
+     #@doc.xpath('//e:eac/e:condesc/e:identity/e:pershead[@authorized!=""]/e:part[@type="surname"]', 'e' => "http://xml.ra.se/EAC")[0].content=name
+  end
+
+  def startdate
+     if is_new?
+       return nil
+     end
+     #@doc.xpath('//e:eac/e:condesc/e:identity/e:pershead[@authorized!=""]/e:part[@type="surname"]', 'e' => "http://xml.ra.se/EAC")[0].text
+     ''
+  end
+
+  def enddate= enddate
+     if is_new?
+       @doc = create_empty
+     end
+     
+     #@doc.xpath('//e:eac/e:condesc/e:identity/e:pershead[@authorized!=""]/e:part[@type="surname"]', 'e' => "http://xml.ra.se/EAC")[0].content=name
+  end
+
+  def enddate
+     if is_new?
+       return nil
+     end
+     #@doc.xpath('//e:eac/e:condesc/e:identity/e:pershead[@authorized!=""]/e:part[@type="surname"]', 'e' => "http://xml.ra.se/EAC")[0].text
+     ''
+  end
+
+  def biography= biography
+     if is_new?
+       @doc = create_empty
+     end
+     
+     #@doc.xpath('//e:eac/e:condesc/e:identity/e:pershead[@authorized!=""]/e:part[@type="surname"]', 'e' => "http://xml.ra.se/EAC")[0].content=name
+  end
+
+  def biography
+     if is_new?
+       return nil
+     end
+     #@doc.xpath('//e:eac/e:condesc/e:identity/e:pershead[@authorized!=""]/e:part[@type="surname"]', 'e' => "http://xml.ra.se/EAC")[0].text
+     ''
+  end
+
+  # create an empty eac document
+  def create_empty
+     EacGenerator.generate_empty('person')
+  end
+end
