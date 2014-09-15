55bf7df5cf6cf797eb64e1d518cd4f62983f59f5
Leonard Norberg <leonard.norberg@sunstone.se>
2014-09-15 15:41:03 +0200

    Fixes for DC in the new attr based implementation

--- app/models/authority.rb
+++ app/models/authority.rb
@@ -1,11 +1,11 @@
 class Authority < DarcFedora
 
-  #attr_datastream :dc, :title
+  attr_datastream :dc, :title
   attr_datastream :eac, :authorized_forename, :authorized_surname, :startdate, :enddate
   scope :brief, :authorized_forename, :startdate
-  scope :full, :authorized_forename, :authorized_surname, :startdate, :enddate
+  scope :full, :title, :authorized_forename, :authorized_surname, :startdate, :enddate
 
-  def initialize id, obj, scope="brief"
+  def initialize id, obj, scope="full"
     super
     load
   end
