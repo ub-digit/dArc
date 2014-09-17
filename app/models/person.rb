93ecaf26313f14532f4a3ea9701710bfdfe08fe0
Jimmy Carlsson <jimmy.carlsson@ub.gu.se>
2014-09-17 14:16:30 +0200

    Commented darc_fedora

--- app/models/person.rb
+++ app/models/person.rb
@@ -5,8 +5,8 @@ attr_datastream :eac, :authorized_forename, :authorized_surname, :startdate, :en
 scope :brief, :authorized_forename, :startdate
 scope :full, :title, :authorized_forename, :authorized_surname, :startdate, :enddate
 
-  def self.fedora_model_name
-     'Authority'
-  end
+def self.fedora_model_name
+ 'Authority'
+end
 
 end
