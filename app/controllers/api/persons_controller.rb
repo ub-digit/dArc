1114b1756d433d77ba1d3d4ccefb5c2cf0e59b0b
Per Larsson <per.larsson@ub.gu.se>
2014-09-17 15:42:50 +0200

    Added controller for persons.

---
+++ app/controllers/api/persons_controller.rb
@@ -0,0 +1,38 @@
+class Api::PersonsController < Api::ApiController
+
+
+  def index
+    @authorities = Person.all({:select => :full})
+    if !@authorities.nil?
+      render json: {persons: @authorities}, status: 200
+    else
+      render json: {error: "No objects found"}, status: 404
+    end
+  rescue => error
+    render json: {error: "No objects found"}, status: 404
+  end
+
+  def show
+    @authority = Person.find(params[:id].to_i, {:select => :full})
+    if !@authority.nil? 
+      render json: {person: @authority}, status: 200
+    else
+      render json: {error: "Could not find object with id: #{params[:id]}"}, status: 404
+    end
+  rescue => error
+    render json: {error: "Could not find object with id: #{params[:id]}"}, status: 404
+  end
+
+  def update
+    @authority = Person.find(params[:id].to_i, {:select => :full})
+    if !@authority.nil? 
+      @authority.from_json(request.raw_post)
+      @authority.save
+      render json: {person: @authority}, status: 200
+    else
+      render json: {error: "Could not find object with id: #{params[:id]}"}, status: 404
+    end
+  rescue => error
+    render json: {error: "Could not find object with id: #{params[:id]}"}, status: 404
+  end
+end
---
+++ spec/controllers/persons_controller_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+RSpec.configure do |c|
+	c.include TestHelper
+end
+
+RSpec.describe Api::PersonsController, :type => :controller do
+	before :each do
+		config_init
+		@api_key = Rails.application.config.api_key
+	end
+
+	describe "GET show" do 
+		context "with existing id" do 
+			it "returns a json message" do 
+				get :show, api_key: @api_key, id: db_ids[:authority]
+				expect(response.status.to_i == 200).to be true
+			end 
+		end
+		context "with a non existing id" do 
+			it "returns a error message" do 
+				get :show, api_key: @api_key, id: db_ids[:invalid]
+				expect(response.status.to_i == 404).to be true
+			end 
+		end
+	end
+
+	describe "GET index" do
+		context "for a valid model" do
+			it "should return a list of objects" do
+				get :index, api_key: @api_key
+				expect(json["persons"]).to_not be nil
+				expect(json["persons"]).to be_an(Array)
+			end
+		end
+	end
+
+	describe "PUT update" do
+		context "with valid attributes" do
+			it "should return a success message" do
+				post :update, api_key: @api_key, id: db_ids[:authority]
+				expect(response.status.to_i == 200).to be true
+			end
+		end
+	end
+end
