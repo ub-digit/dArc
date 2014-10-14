f11bd609da4a867acbe0b72ac2e0e871424147cc
Per Larsson <per.larsson@ub.gu.se>
2014-10-14 16:37:55 +0200

    Added attribute archives to create scope for Disk

--- app/models/disk.rb
+++ app/models/disk.rb
@@ -4,6 +4,7 @@ class Disk < DarcFedoraObject
   attr_datastream :relations_in_subset, :disk_images
   scope :full, :archives, :disk_images
   scope :update, :archives
+  scope :create, :archives
 
   def initialize id, obj, scope="brief"
      super
