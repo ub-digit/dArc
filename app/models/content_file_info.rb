ea239394bb07fc0420d891117e6c3569e705a021
Leonard Norberg <leonard.norberg@sunstone.se>
2014-10-17 14:41:46 +0200

    Re-use a mongodb collection between saves for performance reasons

--- app/models/content_file_info.rb
+++ app/models/content_file_info.rb
@@ -3,16 +3,18 @@ require 'mongo'
 class ContentFileInfo
   attr_reader :hash
 
+  @@mongo_coll = nil
+
   def initialize hash
     @hash = hash
   end
   
   def save
     unless @hash == nil
-      client = Mongo::MongoClient.new # defaults to localhost:27017
-      db     = client['darc-content']
-      coll   = db['dfxml']
-      coll.insert(@hash)
+      #client = Mongo::MongoClient.new # defaults to localhost:27017
+      #db     = client['darc-content']
+      #coll   = db['dfxml']
+      mongo_collection.insert(@hash)
     end
   end
   
@@ -24,5 +26,14 @@ class ContentFileInfo
       docs = coll.find({'disk_image' => disk_image_id, 'parent_object' => parent_id})    
 
       docs
+  end
+
+  def mongo_collection
+      return @@mongo_coll unless @@mongo_coll == nil
+
+      client = Mongo::MongoClient.new # defaults to localhost:27017
+      db     = client['darc-content']
+      @@mongo_coll   = db['dfxml']
+      @@mongo_coll
   end
 end
